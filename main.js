5d042e3e7a5f48fd092c0743c171dd7d012fd57f
brady pierce <bradypierce13@gmail.com>
2023-11-27 11:58:18 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/moon.css";
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown";
+
+let deck = new Reveal({ plugins: [Markdown] });
+deck.initialize();
